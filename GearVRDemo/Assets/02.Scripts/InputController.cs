bcc8283fa38c84565ec07da37150d056e221052a
Jaehyun Lee <myleje@gmail.com>
2019-08-26 15:40:39 +0900

    .

--- GearVRDemo/Assets/02.Scripts/InputController.cs
+++ GearVRDemo/Assets/02.Scripts/InputController.cs
@@ -7,9 +7,16 @@ public class InputController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //트리거 버튼 클릭
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
             Debug.Log("Trigger Clicked !!!");
+        }
+        //트랙패드 터치
+        if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        {
+            Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+            Debug.LogFormat("Touch Position x={0}, y={1}", pos.x, pos.y);
         }
     }
 }
