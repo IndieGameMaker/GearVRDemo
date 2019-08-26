0a1a32d24b2fef290c6e0e04b9fa03c0fe7e3f20
Jaehyun Lee <myleje@gmail.com>
2019-08-26 15:28:39 +0900

    .

---
+++ GearVRDemo/Assets/02.Scripts/InputController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("Trigger Clicked !!!");
+        }
+    }
+}
